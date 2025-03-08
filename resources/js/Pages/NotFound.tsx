e42073e5e1027f05ad75064ca069b79e99363f22
Sergey Bolshakov <serg.bolshakov@gmail.com>
2025-03-08 17:32:36 +0300

    Корректировка текста файла NotFound.tsx

--- resources/js/Pages/NotFound.tsx
+++ resources/js/Pages/NotFound.tsx
@@ -1,9 +1,12 @@
 // resources/js/Pages/NotFound.tsx
+import { Link } from '@inertiajs/react';
+
 export default function NotFound() {
     return (
         <div>
             <h1>404 - Страница не найдена</h1>
             <p>Извините, запрашиваемой страницы не существует...</p>
+            <Link href="/">Вернуться на главную</Link>
         </div>
     );
 }
