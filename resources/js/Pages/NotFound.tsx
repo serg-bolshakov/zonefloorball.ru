3f1c9c6388bc12c9172e4ebc839d87cd9e3498c2
Sergey Bolshakov <serg.bolshakov@gmail.com>
2025-03-08 16:51:35 +0300

    Реализовал минимальный компонент NotFound

---
+++ resources/js/Pages/NotFound.tsx
@@ -0,0 +1,9 @@
+// resources/js/Pages/NotFound.tsx
+export default function NotFound() {
+    return (
+        <div>
+            <h1>404 - Страница не найдена</h1>
+            <p>Извините, запрашиваемая страница не существует.</p>
+        </div>
+    );
+}
