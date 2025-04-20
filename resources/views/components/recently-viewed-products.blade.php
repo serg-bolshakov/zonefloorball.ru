607983ef76153341a79a1745cd8edf86dae0adbc
Sergey Bolshakov <serg.bolshakov@gmail.com>
2025-04-20 07:41:04 +0300

    some files were deleted from the project

--- resources/views/components/recently-viewed-products.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="page-products__viewed-content">
-    <div class="product-card__recently-viewed">
-        <script src="{{ asset('js/recently-viewed.js') }}"></script>
-    </div>            
-</div>
-
