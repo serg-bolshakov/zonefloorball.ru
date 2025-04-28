54320ed0d8a6e75ec7665fbcb86ef3d1ce06ebcb
Sergey Bolshakov <serg.bolshakov@gmail.com>
2025-04-28 22:37:58 +0300

    изменение cartPage и DeliverySelector... num 2

--- resources/js/Components/Cart/DeliverySelector.tsx
+++ resources/js/Components/Cart/DeliverySelector.tsx
@@ -1,8 +1,9 @@
 //resources/js/Components/Cart/DeliverySelector.tsx
 
 import { useState } from 'react';
+import { usePage } from '@inertiajs/react';
 import RussianPostMap from '../RussianPostMap';
-import type { ITransport } from '@/Types/delivery';
+import { ITransport } from '@/Types/delivery';
 
 interface DeliverySelectorProps {
   transports: ITransport[];
@@ -14,10 +15,58 @@ interface DeliverySelectorProps {
   }) => void;
 }
 
+interface DeliveryWay {
+  id: number;
+  name: string;
+  price: number;
+  type: 'pickup' | 'local' | 'post';
+}
+
+
+interface onDeliveryChangeProps {
+    onDeliveryChange: (deliveryWayId: number, price: number) => void;
+}
+
+interface onPostOfficeSelectProps {
+  onPostOfficeSelect: (deliveryAddress: string, deliveryPrice: number, deliveryTime: string) => void;
+}
+
+// interface DeliverySelectorProps {
+//   onDeliveryChange: (optionId: number, price: number) => void;
+//   onPostOfficeSelect: (address: string, price: number, time: string) => void;
+// }
+
 const DeliverySelector = ({ transports, onSelect }: DeliverySelectorProps) => {
   const [selectedTransportId, setSelectedTransportId] = useState<number>(0);
-  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
-  const selectedTransport = transports.find(t => t.id === selectedTransportId);
+  const { props } = usePage();  // Автоматически получит тип из inertia.d.ts
+                                // хук из библиотеки @inertiajs/react - предоставляет доступ к:
+                                // Пропсам, переданным от сервера (Laravel) / URL-параметрам / Данным сессии
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [postDeliveryPrice, setPostDeliveryPrice] = useState<number>(0);
+
+  // Варианты доставки (можно вынести в конфиг или получать из API)
+  /*const deliveryWays: DeliveryWay[] = [
+    { id: 1, name: 'Самовывоз со склада продавца: ', price: 0, type: 'pickup' },
+    { id: 2, name: 'Доставка по Нижнему Новгороду: ', price: props.deliveryPrice ?? 990, type: 'local' },
+    { id: 3, name: 'Почта России: ', price: props.deliveryPrice ?? 0, type: 'post' } // Цена будет уточнена
+  ];*/
+
+  /*const handleDeliveryWaySelect = (deliveryWayId: number, deliveryPrice: number) => {
+    onDeliveryChange(deliveryWayId, deliveryPrice); // Передаём данные в родительский компонент
+    setSelectedDeliveryWay(deliveryWayId);
+    setIsDropdownOpen(false);
+  };*/
+
+  /*const handlePostOfficeData = (data: {
+    address: string;
+    cost: number;
+    deliveryTime: string;
+    postOfficeId: number;
+  }) => {
+    onDeliveryChange(3, data.cost); // 3 - ID способа "Почта России"
+    onPostOfficeSelect(data.address, data.cost, data.deliveryTime);
+    setPostDeliveryPrice(data.cost);
+  };*/
 
   return (
     <>      
@@ -56,18 +105,17 @@ const DeliverySelector = ({ transports, onSelect }: DeliverySelectorProps) => {
                   onClick={() => {
                     setSelectedTransportId(transport.id);
                     setIsDropdownOpen(false);
-                  }}
-                >
+                  }}>
                 
                   <a data-transport={transport.id}>
                   {transport.id === 3 ? (
                       <> {transport.name} 
-                      {`от 490 ₽`}
+                      {`: от 490 ₽`}
                       </>
                     ) : (
                       <>{transport.name} 
-                      {transport.base_price > 0 && `${transport.base_price} ₽`}
-                      {transport.base_price === 0 && 'бесплатно'} </>
+                      {transport.base_price > 0 && `: ${transport.base_price} ₽`}
+                      {transport.base_price === 0 && ': бесплатно'} </>
                   )}
                   </a>
 
@@ -87,22 +135,21 @@ const DeliverySelector = ({ transports, onSelect }: DeliverySelectorProps) => {
         {selectedTransportId === 3 && (
           <>
             <div className="russianpost-map__content">
-                {/* {postDeliveryPrice === 0 && (
+                {postDeliveryPrice === 0 && (
                   <p className="russianpost-map__content-text">
                     Для просмотра сроков и стоимости доставки заказа, введите адрес и выберите отделение связи:
                 </p>
-                )} */}
-                
+                )}
                 <RussianPostMap 
                   onSelect={(data) => onSelect({
                     transportId: 3,
                     address: data.address,
                     price: data.cost,
                     time: data.deliveryTime
-                  })}
+                })}
                 />
             </div>
-            {/* {postDeliveryPrice > 0 && (
+            {postDeliveryPrice > 0 && (
               <>
                 <div className="basket-res__delivery">
                   <h3 className="basketPriceYesDeliveryBlockH3elemDelivery">
@@ -111,7 +158,7 @@ const DeliverySelector = ({ transports, onSelect }: DeliverySelectorProps) => {
                   </h3>
               </div>
               </>
-            )} */}
+            )}
           </>
         )}       
     </>
