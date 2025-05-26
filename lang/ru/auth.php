2284d853f6d3101cfb587d5b7510dc299736195c
Sergey Bolshakov <serg.bolshakov@gmail.com>
2025-05-26 19:00:10 +0300

    Добавлены русские переводы

---
+++ lang/ru/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Введённые пароль и логин либо не совпадают, либо такие записи отсутствуют в наше базе данных... Попробуйте ещё раз...',
+    'password' => 'Введён неверный пароль.',
+    'throttle' => 'Слишком много попыток авторизации. Пожалуйста попробуйте ещё раз не ранее, чем через :seconds секунд.',
+
+];
---
+++ lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль был изменён.',                                               // 'Your password has been reset.',
+    'sent' => 'Мы отправили вам ссылку для изменения пароля.',                          // 'We have emailed your password reset link.',
+    'throttled' => 'Пожалуйста, подождите перед повторной попыткой.',                   // 'Please wait before retrying.',
+    'token' => 'Этот токен сброса пароля недействителен.',                              // 'This password reset token is invalid.',
+    'user' =>  'Мы не можем найти пользователя с таким адресом электронной почты.',     // "We can't find a user with that email address.",
+
+];
