86c74758950183d995779c59b3fb3236550a8ef8
Sergey Bolshakov <serg.bolshakov@gmail.com>
2025-07-16 16:43:59 +0300

    заюзал в модели Order use Illuminate\Support\Facades\DB

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log; 
 use Illuminate\Database\Eloquent\Model;
 
 use App\Enums\OrderStatus;
