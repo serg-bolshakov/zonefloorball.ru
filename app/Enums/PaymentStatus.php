6e1f31c44553bac546fb553651bb9d9c1bc7979c
Sergey Bolshakov <serg.bolshakov@gmail.com>
2025-07-18 08:45:43 +0300

    Обновил таблицу статусов оплаты

--- app/Enums/PaymentStatus.php
+++ app/Enums/PaymentStatus.php
@@ -4,20 +4,24 @@ namespace App\Enums;
 
 enum PaymentStatus: string {
     case PENDING = 'pending';
+    case INITIATED = 'initiated';
     case PARTIALLY_PAID = 'partially_paid';
     case PAID = 'paid';
     case FAILED = 'failed';
     case PARTIALLY_REFUNDED = 'partially_refunded';
     case REFUNDED = 'refunded';
+    case CANCELLED = 'cancelled';
     
     public function label(): string {
         return match($this) {
-            self::PENDING => 'Ожидает оплаты',
-            self::PARTIALLY_PAID => 'Частично оплачен',
-            self::PAID => 'Оплачен',
-            self::FAILED => 'Ошибка оплаты',
-            self::PARTIALLY_REFUNDED => 'Частичный возврат',
-            self::REFUNDED => 'Возврат'
+            self::PENDING               => 'Ожидает оплаты',
+            self::INITIATED             => 'Операция оплаты инициализирована',  // деньги от покупателя не получены. Или от пользователя ещё не поступила оплата по выставленному ему счёту или платёжная система, через которую пользователь совершает оплату, ещё не подтвердила факт оплаты.
+            self::PARTIALLY_PAID        => 'Частично оплачен',
+            self::PAID                  => 'Оплачен',
+            self::FAILED                => 'Ошибка оплаты',                     // Отказ в зачислении средств на счёт магазина. Списанные средства вернулись покупателю на счёт (кошелёк), с которого производилась оплата. 
+            self::PARTIALLY_REFUNDED    => 'Частичный возврат',
+            self::REFUNDED              => 'Возврат',
+            self::CANCELLED             => 'Отмена оплаты'                      // Операция отменена, деньги от покупателя не были получены. Оплата не была произведена. Покупатель отказался от оплаты или не совершил платеж, и операция отменилась по истечении времени ожидания. Либо платёж был совершён после истечения времени ожидания. 
         };
     }
 
