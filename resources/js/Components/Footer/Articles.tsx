2d815189f8dcccb46f5454948624a32595fe9914
Sergey Bolshakov <serg.bolshakov@gmail.com>
2025-10-27 10:38:25 +0300

    Улучшение блока Футера - статьи

--- resources/js/Components/Footer/Articles.tsx
+++ resources/js/Components/Footer/Articles.tsx
@@ -1,6 +1,7 @@
 // resources/js/Components/Footer/Articles.tsx
 
 import React from 'react';
+import { Link } from '@inertiajs/react';
 import useModal from '../../Hooks/useModal';
 import AboutFlex from '../Articles/AboutFlex';
 import AboutSideHand from '../Articles/AboutSideHand';
@@ -13,9 +14,38 @@ const Articles: React.FC = () => {
     
     return (
         <>
-            <p className="modal-link" onClick={() => openModal(<AboutFlex />)}>Про жёсткость (флекс)</p>
-            <p className="modal-link" onClick={() => openModal(<AboutSideHand />)}>Как правильно определить хват</p>
-            <p className="modal-link" onClick={() => openModal(<HowChooseStick />)}>Как выбирать клюшку</p>            
+            {/* Жёсткость клюшки */}
+        <div className="article-group">
+            <h3 className='group-title'>Жёсткость клюшки (флекс)</h3>
+            <p className="modal-link" onClick={() => openModal(<AboutFlex />)}>📖 Теория и объяснение</p>
+            <p className="modal-link">
+                <Link href="/products/card/71303-klyushka-dlya-florbola-aleters-epic-air-concept-33mm-black-white-83cm-left">
+                🎬 Видеообзор с примерами (2:52)
+                </Link>
+            </p>
+        </div>
+
+        {/* Выбор клюшки */}
+        <div className="article-group">
+            <h3 className='group-title'>Подбор клюшки</h3>
+            <p className="modal-link" onClick={() => openModal(<HowChooseStick />)}>📖 Как выбирать по росту</p>
+            <p className="modal-link" onClick={() => openModal(<AboutSideHand />)}>📖 Как определить хват</p>
+            <p className="modal-link">
+                <Link href="/products/card/71303-klyushka-dlya-florbola-aleters-epic-air-concept-33mm-black-white-83cm-left">
+                🎬 Видео: Длина клюшки (1:47)
+                </Link>
+            </p>
+        </div>
+
+        {/* Обслуживание */}
+        <div className="article-group">
+            <h3 className='group-title'>Обслуживание экипировки</h3>
+            <p className="modal-link">
+                <Link href="/products/card/76111-obmotka-dlya-klyushek-aleters-high-quality-black-junior">
+                🎬 Видео: Как заменить обмотку (8:23)
+                </Link>
+            </p>
+        </div>
         </>
     );
 };
